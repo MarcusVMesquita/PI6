b759571e492dce322fc48719e434733a8ecca707
Diego Smagnotto <diegosoares597@gmail.com>
2015-03-27 13:28:12 -0300

    Criada nova View
    Criada View Generica para mensagem em pop up, ainda falta alguns ajustes

--- MySolution/MyParking.DAL/Services/ClienteService.cs
+++ MySolution/MyParking.DAL/Services/ClienteService.cs
@@ -1,17 +1,29 @@
 ﻿using MyParking.DAL.Context;
 using MyParking.DAL.Models;
+using MyParking.Framework;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MyParking.DAL.Services
 {
     public class ClienteService
     {
         private MyParkingContext db = new MyParkingContext();
-        public bool GravaCliente(Cliente cliente)
+        public Result GravaCliente(Cliente cliente)
         {
-            db.Clientes.Add(cliente);
-            db.SaveChanges();
+            Result result = new Result();
+            try
+            {
+                db.Clientes.Add(cliente);
+                db.SaveChanges();
+                result.AdicionaMensagem("Gravação do Cliente Concluída", Result.TipoResult.OK);
+            }
+            catch (Exception ex)
+            {
+                result.AdicionaMensagem(ex.Message, Result.TipoResult.Error);
+            }
 
-            return true;
+            return result;
         }
     }
 }
